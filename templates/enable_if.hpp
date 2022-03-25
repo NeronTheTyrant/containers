6935b1a970b3823d2bcd11a8693bbfded8625bf3
Martin Lebard <lebard.martin@gmail.com>
2022-03-25 21:30:06 +0100

    enable_if, is_integral, equal, lexicographical_compare done

---
+++ templates/enable_if.hpp
@@ -0,0 +1,7 @@
+template <bool Cond, class T = void>
+struct enable_if {};
+
+template <class T>
+struct enable_if<true, T> {
+	typedef T	type;
+};
---
+++ templates/lexicographical_compare.hpp
@@ -0,0 +1,32 @@
+template <class InputIterator1, class InputIterator2>
+bool lexicographical_compare (InputIterator1 first1, InputIterator1 last1,
+							InputIterator2 first2, InputIterator2 last2) {
+	while (first1 != last1) {
+		if (first2 == last2)
+			return false;
+		if (*first1 < *first2)
+			return true;
+		if (*first2 < *first1)
+			return false;
+		first1++;
+		first2++;
+	}
+	return (first2 != last2);
+}
+
+template <class InputIterator1, class InputIterator2, class Compare>
+bool lexicographical_compare (InputIterator1 first1, InputIterator1 last1,
+							InputIterator2 first2, InputIterator2 last2,
+							Compare comp) {
+	while (first1 != last1) {
+		if (first2 == last2)
+			return false;
+		if (comp(*first1, *first2))
+			return true;
+		if (comp(*first2, *first1))
+			return false;
+		first1++;
+		first2++;
+	}
+	return (first2 != last2);
+}
